f258c18d9230e60bb0a1bde2b28e05ae5679eb1e
jrandiny <13517063@std.stei.itb.ac.id>
2020-03-09 12:57:25 +0700

    Add tab to main activity

--- app/src/main/java/id/randiny/simplyautomatic/MainActivity.kt
+++ app/src/main/java/id/randiny/simplyautomatic/MainActivity.kt
@@ -1,12 +1,27 @@
 package id.randiny.simplyautomatic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import id.randiny.simplyautomatic.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Create new action", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
