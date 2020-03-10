e0bb0e670e2df4207788b9e45259d085e9d17736
jrandiny <13517063@std.stei.itb.ac.id>
2020-03-10 23:17:12 +0700

    Add viewmodel in activity

--- app/src/main/java/id/randiny/simplyautomatic/MainActivity.kt
+++ app/src/main/java/id/randiny/simplyautomatic/MainActivity.kt
@@ -1,16 +1,17 @@
 package id.randiny.simplyautomatic
 
-import android.app.Notification
-import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
+import id.randiny.simplyautomatic.data.ModuleConfig
+import id.randiny.simplyautomatic.data.RoutineViewModel
+import id.randiny.simplyautomatic.data.RoutineViewModelFactory
+import id.randiny.simplyautomatic.module.ModuleType
 import id.randiny.simplyautomatic.ui.RoutineListPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -30,15 +31,25 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
+        // Setup VM
+        val routineViewModel =
+            ViewModelProvider(this, RoutineViewModelFactory(this)).get(RoutineViewModel::class.java)
+
+
         // Setup FAB
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Create new action", Snackbar.LENGTH_LONG).show()
+        fab.setOnClickListener {
+            val sampleParam = mutableMapOf<String, Any>()
+            sampleParam.put("sample1", 123)
+            sampleParam.put("sample2", "asd")
+            val trigger = ModuleConfig(ModuleType.TIME, sampleParam)
+            val action = ModuleConfig(ModuleType.API, sampleParam)
+            routineViewModel.addItem(trigger, action)
         }
 
         // Setup service
-        if(!MainService.isInstanceCreated()){
+        if (!MainService.isInstanceCreated()) {
             val intent = Intent(this, MainService::class.java)
             ContextCompat.startForegroundService(this, intent)
         }
