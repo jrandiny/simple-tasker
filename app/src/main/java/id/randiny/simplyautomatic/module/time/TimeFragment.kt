ee92fc8e8afad05b8419ef7606338396707ba58a
tancos02 <paulussiahaan02@gmail.com>
2020-03-12 19:50:15 +0700

    Fixing time fragment view

--- app/src/main/java/id/randiny/simplyautomatic/module/time/TimeFragment.kt
+++ app/src/main/java/id/randiny/simplyautomatic/module/time/TimeFragment.kt
@@ -6,14 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import id.randiny.simplyautomatic.R
+import id.randiny.simplyautomatic.R.id.radioButton5
 
 /**
  * A simple [Fragment] subclass.
  * Use the [TimeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TimeFragment : Fragment() {
+class TimeFragment : Fragment(), RadioGroup.OnCheckedChangeListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,50 +24,29 @@ class TimeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_time, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-            if(view is RadioButton) {
-                val checked = view.isChecked
-                when(view.getId()){
-                    R.id.radioButton5 ->
-                        if(checked) {
-                            when(view.getId()){
-                                R.id.textView1 ->
-                                    view.visibility = View.VISIBLE
-                            }
-                            when(view.getId()){
-                                R.id.day_picker ->
-                                    view.visibility = View.VISIBLE
-                            }
-                            when(view.getId()){
-                                R.id.textView ->
-                                    view.visibility = View.GONE
-                            }
-                            when(view.getId()){
-                                R.id.datePicker ->
-                                    view.visibility = View.GONE
-                            }
-                        }
-                    R.id.radioButton6 ->
-                        if(checked) {
-                            when(view.getId()){
-                                R.id.textView1 ->
-                                    view.visibility = View.GONE
-                            }
-                            when(view.getId()){
-                                R.id.day_picker ->
-                                    view.visibility = View.GONE
-                            }
-                            when(view.getId()){
-                                R.id.textView ->
-                                    view.visibility = View.VISIBLE
-                            }
-                            when(view.getId()){
-                                R.id.datePicker ->
-                                    view.visibility = View.VISIBLE
-                            }
-                        }
-                }
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        val checkedRadioButton = group?.findViewById<RadioButton>(group.checkedRadioButtonId)
+        if (checkedRadioButton != null) {
+            if(checkedRadioButton.id == R.id.radioButton5) {
+                val text1 = view!!.findViewById<TextView>(R.id.textView1)
+                text1.visibility = View.VISIBLE
+                val dayPicker = view!!.findViewById<TextView>(R.id.day_picker)
+                dayPicker.visibility = View.VISIBLE
+                val text = view!!.findViewById<TextView>(R.id.textView)
+                text1.visibility = View.GONE
+                val datePicker = view!!.findViewById<TextView>(R.id.datePicker)
+                datePicker.visibility = View.GONE
             }
+            else if(checkedRadioButton.id == R.id.radioButton6) {
+                val text1 = view!!.findViewById<TextView>(R.id.textView1)
+                text1.visibility = View.GONE
+                val dayPicker = view!!.findViewById<TextView>(R.id.day_picker)
+                dayPicker.visibility = View.GONE
+                val text = view!!.findViewById<TextView>(R.id.textView)
+                text1.visibility = View.VISIBLE
+                val datePicker = view!!.findViewById<TextView>(R.id.datePicker)
+                datePicker.visibility = View.VISIBLE
+            }
+        }
     }
 }
