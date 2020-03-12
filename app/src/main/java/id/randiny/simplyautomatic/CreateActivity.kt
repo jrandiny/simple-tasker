8746b6504647e43f37cd703ddee545846810d035
jrandiny <13517063@std.stei.itb.ac.id>
2020-03-12 11:00:06 +0700

    Implement creating routine from create activity

--- app/src/main/java/id/randiny/simplyautomatic/CreateActivity.kt
+++ app/src/main/java/id/randiny/simplyautomatic/CreateActivity.kt
@@ -2,15 +2,35 @@ package id.randiny.simplyautomatic
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.button.MaterialButton
+import id.randiny.simplyautomatic.data.ModuleConfig
+import id.randiny.simplyautomatic.data.RoutineViewModel
+import id.randiny.simplyautomatic.data.RoutineViewModelFactory
+import id.randiny.simplyautomatic.module.ModuleType
 
-import kotlinx.android.synthetic.main.create_activity.*
 
 class CreateActivity : AppCompatActivity() {
+
+    private lateinit var routineViewModel: RoutineViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_activity)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        routineViewModel =
+            ViewModelProvider(this, RoutineViewModelFactory(this)).get(RoutineViewModel::class.java)
+
+        findViewById<MaterialButton>(R.id.create_btn).setOnClickListener {
+            val sampleParam = mutableMapOf<String, Any>()
+            sampleParam.put("sample1", 123)
+            sampleParam.put("sample2", "asd")
+            val trigger = ModuleConfig(ModuleType.TIME, sampleParam)
+            val action = ModuleConfig(ModuleType.API, sampleParam)
+            routineViewModel.addItem(trigger, action)
+            finish()
+        }
     }
 
 }
--- app/src/main/java/id/randiny/simplyautomatic/MainActivity.kt
+++ app/src/main/java/id/randiny/simplyautomatic/MainActivity.kt
@@ -4,23 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
-import id.randiny.simplyautomatic.data.ModuleConfig
-import id.randiny.simplyautomatic.data.RoutineViewModel
-import id.randiny.simplyautomatic.data.RoutineViewModelFactory
-import id.randiny.simplyautomatic.module.ModuleType
 import id.randiny.simplyautomatic.ui.RoutineListPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private val CREATE_ROUTINE_REQUEST = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Setup tab
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
         val routineListPagerAdapter =
             RoutineListPagerAdapter(
                 this,
@@ -31,20 +29,12 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
-        // Setup VM
-        val routineViewModel =
-            ViewModelProvider(this, RoutineViewModelFactory(this)).get(RoutineViewModel::class.java)
-
         // Setup FAB
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener {
-            val sampleParam = mutableMapOf<String, Any>()
-            sampleParam.put("sample1", 123)
-            sampleParam.put("sample2", "asd")
-            val trigger = ModuleConfig(ModuleType.TIME, sampleParam)
-            val action = ModuleConfig(ModuleType.API, sampleParam)
-            routineViewModel.addItem(trigger, action)
+            val intent = Intent(this, CreateActivity::class.java)
+            startActivity(intent)
         }
 
         // Setup service
