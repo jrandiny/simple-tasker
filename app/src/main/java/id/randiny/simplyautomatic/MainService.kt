3bfa4b9c1c8d59ea67698e978925c74b2146d1b9
jrandiny <13517063@std.stei.itb.ac.id>
2020-03-12 22:25:38 +0700

    Implement module loading and invocation on service

--- app/src/main/java/id/randiny/simplyautomatic/MainService.kt
+++ app/src/main/java/id/randiny/simplyautomatic/MainService.kt
@@ -10,21 +10,43 @@ import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.lifecycle.Observer
+import id.randiny.simplyautomatic.data.MainDatabase
+import id.randiny.simplyautomatic.data.Routine
+import id.randiny.simplyautomatic.data.RoutineDAO
+import id.randiny.simplyautomatic.module.Module
+import id.randiny.simplyautomatic.module.ModuleFactory
 
 class MainService : Service() {
-    private val SERVICE_NOTIFICATION_ID = 1
-    private val SERVICE_CHANNEL_ID = "Service"
-    private val SERVICE_CHANNEL_NAME = "Service Notification"
-    private val LOG_TAG = "My/Service"
+    private lateinit var mainDatabase: MainDatabase
+    private lateinit var routineDAO: RoutineDAO
 
+    private val condition: MutableList<Module> = mutableListOf()
+    private val action: MutableList<Module> = mutableListOf()
+
+    private val routineObserver = Observer<List<Routine>> {
+        cleanupService()
+        condition.clear()
+        action.clear()
+        Log.d(LOG_TAG, "Get ${it.size} new routines")
+        for (routine in it) {
+            condition.add(ModuleFactory.createModule(routine, true, this))
+            action.add(ModuleFactory.createModule(routine, false, this))
+        }
+        prepareService()
+    }
 
     companion object {
         private var INSTANCE: MainService? = null
         fun isInstanceCreated(): Boolean {
             return INSTANCE != null
         }
-    }
 
+        private const val SERVICE_NOTIFICATION_ID = 1
+        private const val SERVICE_CHANNEL_ID = "Service"
+        private const val SERVICE_CHANNEL_NAME = "Service Notification"
+        private const val LOG_TAG = "My/Service"
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -50,14 +72,35 @@ class MainService : Service() {
             .setPriority(NotificationCompat.PRIORITY_MIN)
             .build()
 
+        mainDatabase = MainDatabase.getDatabase(applicationContext)
+        routineDAO = mainDatabase.routineDAO()
+
+        routineDAO.getAllActive().observeForever(routineObserver)
+
         startForeground(SERVICE_NOTIFICATION_ID, notification)
         Log.d(LOG_TAG, "Service start")
         INSTANCE = this
+
         return START_STICKY
     }
 
     override fun onDestroy() {
         INSTANCE = null
+        routineDAO.getAllActive().removeObserver(routineObserver)
         Log.d(LOG_TAG, "Service destroyed")
+    }
+
+    private fun cleanupService() {
+        Log.d(LOG_TAG, "Cleaning up module")
+        for (module in condition) {
+            module.destroyListener()
+        }
+    }
+
+    private fun prepareService() {
+        Log.d(LOG_TAG, "Preparing ${condition.size} modules")
+        for (i in 0 until condition.size) {
+            condition[i].setupListener(action[i])
+        }
     }
 }
