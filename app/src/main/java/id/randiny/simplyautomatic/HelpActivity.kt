9511e9cb123944b774194db7501c98904e51046d
jrandiny <13517063@std.stei.itb.ac.id>
2020-03-13 19:16:37 +0700

    Update help activity button

--- app/src/main/java/id/randiny/simplyautomatic/HelpActivity.kt
+++ app/src/main/java/id/randiny/simplyautomatic/HelpActivity.kt
@@ -1,16 +1,14 @@
 package id.randiny.simplyautomatic
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import kotlinx.android.synthetic.main.activity_help.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.help_activity.*
 
 class HelpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_help)
+        setContentView(R.layout.help_activity)
 
         // Setup Web View
         web_view.loadUrl("file:///android_asset/help.html")
